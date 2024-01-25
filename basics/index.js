b9860d9bafa6378da80b30e3eec0527a1dd5ef0f
shruthi reddy <shruthireddy583@gmail.com>
2024-01-25 13:13:50 +0530

    index

---
+++ basics/index.js
@@ -0,0 +1,20 @@
+var brand="PUMA"
+
+var  pName="girls shoes"
+
+var availability= true
+
+var price= 2000
+
+var string= 4.5
+var dicsount;
+var size=[38,40,42,44]
+var location= null
+//create
+var size=(38,40,42,44)
+// read
+console.log(brand)
+// update
+brand="nike"
+console.log(brand)
+
