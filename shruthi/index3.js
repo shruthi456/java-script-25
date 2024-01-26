21b0812070750e8e394921707982641b1468a245
shruthi reddy <shruthireddy583@gmail.com>
2024-01-26 15:46:23 +0530

    .

---
+++ shruthi/index3.js
@@ -0,0 +1,11 @@
+//Hour
+//If hour is between 6am and 12pm: good morning!
+//If it is between 12am and 6am: good afternoon!
+//Otherwise: goodeveng!
+let hour = 1;
+if(hour>=6 && hour < 12){
+console.log('good morning');
+}
+else{
+    console.log('good afternoon')
+}
