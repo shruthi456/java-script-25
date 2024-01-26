12209322210dcd5bbbcd48d414cfe12ff528e7b4
shruthi reddy <shruthireddy583@gmail.com>
2024-01-26 15:34:40 +0530

    .

---
+++ shruthi/index2.js
@@ -0,0 +1,22 @@
+var a=200
+var b=300
+var c="30"
+var d=40.5
+var e="charan"
+var f="reddy"
+var g=true
+var h=false
+var i;
+
+console.log (a+b) 
+console.log (a+c)
+console.log (a+d)
+console.log (a+e)
+console.log (a+f)
+console.log  (a+g)
+console.log (a+h)
+console.log (a+i)
+console.log (e+f)
+console.log (f+i)
+
+
---
+++ shruthi/shru.js
@@ -0,0 +1,10 @@
+var esal=45000.00
+var eSal=45000.01
+var forEach="banglore"
+var elseif = "abc"
+
+console.log(elseif);
+console.log(eSal);
+console.log(forEach);
+//read
+console.log(elseif)
