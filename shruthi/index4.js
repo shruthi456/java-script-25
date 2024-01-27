df6cd00b1deb794fdddaf841bc60f1bf3d033ada
shruthi reddy <shruthireddy583@gmail.com>
2024-01-27 10:19:03 +0530

    .

---
+++ shruthi/index4.js
@@ -0,0 +1,11 @@
+// program to check if the number is even or odd
+// take input from thr user
+ var number =(3)
+//check if the number is even
+if(number % 2== 0){
+    console.log("the number is even.");
+} 
+//if the number is odd
+else{
+    console.log("the number is odd.");
+}
