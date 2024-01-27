a961ce76e260c73fa250445f4d7d08d45bf1fde6
shruthi reddy <shruthireddy583@gmail.com>
2024-01-27 11:55:31 +0530

    .

---
+++ shruthi/index6.js
@@ -0,0 +1,22 @@
+var number = 100
+    if(100<= 33){
+        console.log('you are failed');
+    }
+    else if(100<=101){
+        console.log("your grade: D");
+    } else if(100<=50){
+        console.log("your grade: C");
+    } else if(100>=60){
+        console.log("your grade: B");
+    } else if(100<=70){
+        console.log("your grade: A");
+    } else if(100<=80){
+        console.log("your grade: A+");
+    }else {
+        console.log('you are failed');
+    }
+
+
+    
+    
+    
