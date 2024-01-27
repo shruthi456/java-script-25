0deca38f8a22941733fa2d9c8ae3f88ff7310257
shruthi reddy <shruthireddy583@gmail.com>
2024-01-27 11:12:15 +0530

    .

---
+++ shruthi/index5.js
@@ -0,0 +1,12 @@
+var day ="tuesday";
+if (day ==='monday'){
+    dayvalue = 0;
+}
+else if (day === 'tuesday'){
+    dayvalue = 1;
+}
+else if (day === 'wednesday'){
+    dayvalue = 2;
+}
+
+console.log('monday')
