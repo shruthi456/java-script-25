e62fcb6cba957ec07d9cf8284ac87543e8ef8ae3
shruthi reddy <shruthireddy583@gmail.com>
2024-01-29 12:45:13 +0530

    .

---
+++ shruthi/multiple.js
@@ -0,0 +1,8 @@
+// write a program to check if a number is multiple of 3 or not
+let num=30
+if(num>2){
+    console.log("yes! it is mutliple of 3")
+}
+else{
+console.log("no!it is not a multiple of 3")
+}
