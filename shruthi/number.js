a57642fb0f4eff75ea616fada55b94408df1b09e
shruthi reddy <shruthireddy583@gmail.com>
2024-01-29 18:52:31 +0530

    .

---
+++ shruthi/number.js
@@ -0,0 +1,8 @@
+//write a program to check if a number is 3-digit numbers or not 
+let num=789
+if(num>=100 && num<999){
+    console.log("yes! givin number is 3 digit ")
+}
+else{
+    console.log("no! not a 3 digit number")
+}
