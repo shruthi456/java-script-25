87ecaee8ae3b46d1ed360a0ab71c3a85ee4977d0
shruthi reddy <shruthireddy583@gmail.com>
2024-01-29 12:22:31 +0530

    .

---
+++ shruthi/index9.js
@@ -0,0 +1,8 @@
+//write a program to check if a number is divisible by 7 or not?
+let num = 49
+if(num<70){
+    console.log("yes, it is divisible by 7")
+}
+else{
+   console.log ("no, it is not divisible by 7")
+}
