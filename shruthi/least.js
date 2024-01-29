2f4e5551f4ced29ceb584b952edd89b3e16471d7
shruthi reddy <shruthireddy583@gmail.com>
2024-01-29 20:08:11 +0530

    .

---
+++ shruthi/least.js
@@ -0,0 +1,9 @@
+//write a program to print the least number in given two numbers?
+let n1 = 5;
+ let n2 = 8;
+if(n1,n2<6){
+    console.log("n1 is least")
+}
+else{
+    console.log("n2 is least")
+}
