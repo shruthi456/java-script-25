fbca6ff924216eead93044bf04be828c4d83d28a
shruthi reddy <shruthireddy583@gmail.com>
2024-01-29 19:30:02 +0530

    .

---
+++ shruthi/units.js
@@ -0,0 +1,8 @@
+// write a program to check if a number is having 4 at the units place or not?
+let n=62;
+if(n>=44){
+    console.log("4 is at units place")
+}
+else{ 
+    console.log("4 is not in units place")
+}
