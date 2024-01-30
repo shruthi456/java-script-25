fa6e3580bc85459749919e77aeb37fe3ee50d9a4
shruthi reddy <shruthireddy583@gmail.com>
2024-01-30 13:59:57 +0530

    .

--- shruthi/3num.js
+++ shruthi/3num.js
@@ -1,10 +1,12 @@
+
+//write a program to print the greatest number in given three numbers?
 var a=3;
 var b=5;
 var c=9;
-if(a,b<3){
+if(a>b && a>c){
     console.log("a is greater")
 }
-else if(b,c<9)
+else if(b>a && b>c)
 {
     console.log("b is greater")
 }
---
+++ shruthi/least3num.js
@@ -0,0 +1,14 @@
+
+//write a program to print the least number in given three numbers?
+var a=5;
+var b=10;
+var c=15;
+if(a<b && a<c){
+    console.log("a is least");
+}
+ else if (b<c && b<a){
+    console.log("b is least");
+}
+else {
+    console.log("c is least");
+}
