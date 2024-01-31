453db7e8a7f933c6819494732286bb62b1c97062
shruthi reddy <shruthireddy583@gmail.com>
2024-01-31 19:52:11 +0530

    .

---
+++ shruthi/array.js
@@ -0,0 +1,24 @@
+//create
+var ids=[101,102,103,104]
+var names=["Shruthi","Nayana","shirisha"]
+var arr=[10,20.5,"rahul",true,undefined,[],{}]
+var idex=[101,102,103,104]
+//read
+
+//how to read array elements?
+console.log(ids[0])//101
+console.log(ids[1])//102
+console.log(ids[2])//103
+console.log(ids[3])//104
+console.log(ids[4])//undefined
+console.log(names)
+
+//update
+names[0]="divya"
+console.log(names)
+console.log(names[0])
+
+//delete
+
+names.pop()
+console.log(names)
