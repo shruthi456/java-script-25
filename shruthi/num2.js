f9541211928123f16e287e6010c39b5603302503
shruthi reddy <shruthireddy583@gmail.com>
2024-01-31 12:17:36 +0530

    .

---
+++ shruthi/num2.js
@@ -0,0 +1,4 @@
+// write a program to print 5 to 15 numbers using for loop
+for(let i=5;i<=15;i++){
+    console.log(i)
+}
---
+++ shruthi/num3.js
@@ -0,0 +1,4 @@
+// write a program to print 15 to 10 numbers using for loop
+for(let i=15;i>=10;i--){
+    console.log(i)
+}
