178e669c58c357d1daca7b473b39b7a481119026
shruthi reddy <shruthireddy583@gmail.com>
2024-02-02 12:36:34 +0530

    .

---
+++ shruthi/function.js
@@ -0,0 +1,62 @@
+//function with parameters
+//function add(a,b){
+  // console.log('gm')
+//}
+//add(10,20)
+
+
+
+//function add(a,b){
+    // console.log(a+b)
+  //}
+  //add(10,20) //30
+  //add(100,200)//300
+  //add(10, )//NaN
+  //add(1,"rahul") //1 rahul
+
+
+
+
+  //function with parameters with default function
+  //function add(a,b,c=1){
+    //console.log(a+b+c)
+  //}
+  //add(10,20,30) //60
+  //add(10,20) //31
+  //add(10,1) //12
+
+
+
+
+  //function with return values
+  //function add(a,b){
+    //return a+b;
+  //}
+  //let r1=add(10,20);
+  //console.log(r1); //30
+
+  //let r2=add(1,2)
+  //console.log(r2); //3
+  //console.log(add(100,200))  
+
+
+ // function add(a,b){
+   // let c=100
+    //let d=200
+    //return a+b+c+d
+  //}
+  //let r1=add(1,2)
+  //console.log(r1)
+
+
+  function cal_age(year){
+  let c_year = 2024
+return c_year  - year
+  }
+ // let dob_year = 2000
+  //let age=cal_age(dob_year)
+  //console.log(age)
+  
+  let dob_year = 1998
+  let age=cal_age(dob_year)
+  console.log(age)
