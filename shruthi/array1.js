e6be56b241c70d58957b88e33f9b694c982861a7
shruthi reddy <shruthireddy583@gmail.com>
2024-02-05 21:14:02 +0530

    .

---
+++ shruthi/array1.js
@@ -0,0 +1,10 @@
+let ids=[101,102,103,104]
+//update
+ids.push(105);
+//console.log(ids)
+ids.unshift(100);
+//console.log(ids)
+ids.shift()
+//console.log(ids);
+ids.pop()
+console.log(ids)
---
+++ shruthi/object1.js
@@ -0,0 +1,24 @@
+/*let emp={
+    'eid':101,
+    'ename':'rahul'     
+    }
+    console.log(emp)
+    console.log(JSON.stringify(emp))*/
+
+
+    /*let emp={
+        'eid':101,
+        ename:"Rahul"
+    }
+        console.log(Object.keys(emp).length)*/
+let person={}
+         if(Object.keys(person).length>0 ){
+            console.log("not empty object")
+         }
+            
+        else{
+            console.log("empty object")
+        }
+        
+
+    
