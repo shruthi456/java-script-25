cd7b72d600f13a0ffc27c67d621ccb92f857e391
shruthi reddy <shruthireddy583@gmail.com>
2024-02-14 13:19:00 +0530

    .

---
+++ oops/three.js
@@ -0,0 +1,28 @@
+class Account{
+    acc_Name;
+    acc_Email;
+         constructor(name,email){
+          console.log("Parent Class Constructor")
+          this.acc_Name = name
+          this.acc_Email = email
+    }
+}
+
+class SA extends Account{
+    acc_Id;
+    acc_Bal;
+    min_Bal = 500;
+    constructor(id,name,email,amount){
+        super(name,email)
+        this.acc_Id = id
+        this.acc_Bal = amount
+    }
+
+get_Bal(){
+    return this.acc_Bal - this.min_Bal
+    }
+} 
+let sal=new SA(101,"Rahul","RG@gmail.com",50000)
+console.log(sal)
+
+console.log(sal.get_Bal())
