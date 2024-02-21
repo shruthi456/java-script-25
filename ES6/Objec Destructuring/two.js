24f3a0ee69de61f363dd8e67b442dbb6d9b9d8d5
shruthi reddy <shruthireddy583@gmail.com>
2024-02-21 12:19:41 +0530

    .

---
+++ ES6/Objec Destructuring/two.js
@@ -0,0 +1,7 @@
+const profile = ["Kumar","Swami","JDS"]
+
+const [firstName , lastName , website] = profile;
+
+console.log(firstName);//"Kumar"
+console.log(lastName);//"Swami"
+console.log(website);//"JDS"
