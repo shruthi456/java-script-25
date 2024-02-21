8dbc2c00cce89dcb77e1ff28096b718cff7911bf
shruthi reddy <shruthireddy583@gmail.com>
2024-02-21 12:25:35 +0530

    .

---
+++ ES6/Asynchronous/one.js
@@ -0,0 +1,16 @@
+setTimeout(()=>{
+    console.log("Request 1")
+},12000)
+setTimeout(()=>{
+    console.log("Request 2")
+},6000)
+setTimeout(()=>{
+    console.log("Request 3")
+},4000)
+setTimeout(()=>{
+    console.log("Request 4")
+},5000)
+setTimeout(()=>{
+    console.log("Request 5")
+},1000)
+
