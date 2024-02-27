715fe447c7473b7ec73850960f6b782754a226e1
shruthi reddy <shruthireddy583@gmail.com>
2024-02-27 10:00:30 +0530

    .

---
+++ ES6/Asynchronous/async n await/data.js
@@ -0,0 +1,12 @@
+let employees=[
+    {id:101,name:"Rahul",salary:45000},
+    {id:102,name:"Priyanka",salary:55000}
+]
+let createEmployee=(emp)=>{
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            let dbflag=
+        })
+    })
+}
